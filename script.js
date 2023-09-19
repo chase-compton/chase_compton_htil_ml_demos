bfc4c78ec469b8e39ca561d19a2ca81231d2364e
cscompton1 <cscompton1@crimson.ua.edu>
2023-09-18 23:10:52 -0500

    done with demo

--- script.js
+++ script.js
@@ -46,12 +46,74 @@ function enableCam() {
   }
 }
 
-function trainAndPredict() {
-  // TODO: Fill this out later in the codelab!
+function predictLoop() {
+  if (predict) {
+    tf.tidy(function () {
+      let videoFrameAsTensor = tf.browser.fromPixels(VIDEO).div(255);
+      let resizedTensorFrame = tf.image.resizeBilinear(
+        videoFrameAsTensor,
+        [MOBILE_NET_INPUT_HEIGHT, MOBILE_NET_INPUT_WIDTH],
+        true
+      );
+
+      let imageFeatures = mobilenet.predict(resizedTensorFrame.expandDims());
+      let prediction = model.predict(imageFeatures).squeeze();
+      let highestIndex = prediction.argMax().arraySync();
+      let predictionArray = prediction.arraySync();
+
+      STATUS.innerText =
+        "Prediction: " +
+        CLASS_NAMES[highestIndex] +
+        " with " +
+        Math.floor(predictionArray[highestIndex] * 100) +
+        "% confidence";
+    });
+
+    window.requestAnimationFrame(predictLoop);
+  }
 }
 
+async function trainAndPredict() {
+  predict = false;
+  tf.util.shuffleCombo(trainingDataInputs, trainingDataOutputs);
+  let outputsAsTensor = tf.tensor1d(trainingDataOutputs, "int32");
+  let oneHotOutputs = tf.oneHot(outputsAsTensor, CLASS_NAMES.length);
+  let inputsAsTensor = tf.stack(trainingDataInputs);
+
+  let results = await model.fit(inputsAsTensor, oneHotOutputs, {
+    shuffle: true,
+    batchSize: 5,
+    epochs: 10,
+    callbacks: { onEpochEnd: logProgress },
+  });
+
+  outputsAsTensor.dispose();
+  oneHotOutputs.dispose();
+  inputsAsTensor.dispose();
+  predict = true;
+  predictLoop();
+}
+
+function logProgress(epoch, logs) {
+  console.log("Data for epoch " + epoch, logs);
+}
+
+/**
+ * Purge data and start over. Note this does not dispose of the loaded 
+ * MobileNet model and MLP head tensors as you will need to reuse 
+ * them to train a new model.
+ **/
 function reset() {
-  // TODO: Fill this out later in the codelab!
+  predict = false;
+  examplesCount.length = 0;
+  for (let i = 0; i < trainingDataInputs.length; i++) {
+    trainingDataInputs[i].dispose();
+  }
+  trainingDataInputs.length = 0;
+  trainingDataOutputs.length = 0;
+  STATUS.innerText = 'No data collected';
+  
+  console.log('Tensors in memory: ' + tf.memory().numTensors);
 }
 
 let dataCollectorButtons = document.querySelectorAll("button.dataCollector");
@@ -62,8 +124,45 @@ for (let i = 0; i < dataCollectorButtons.length; i++) {
   CLASS_NAMES.push(dataCollectorButtons[i].getAttribute("data-name"));
 }
 
+function dataGatherLoop() {
+  if (videoPlaying && gatherDataState !== STOP_DATA_GATHER) {
+    let imageFeatures = tf.tidy(function () {
+      let videoFrameAsTensor = tf.browser.fromPixels(VIDEO);
+      let resizedTensorFrame = tf.image.resizeBilinear(
+        videoFrameAsTensor,
+        [MOBILE_NET_INPUT_HEIGHT, MOBILE_NET_INPUT_WIDTH],
+        true
+      );
+      let normalizedTensorFrame = resizedTensorFrame.div(255);
+      return mobilenet.predict(normalizedTensorFrame.expandDims()).squeeze();
+    });
+
+    trainingDataInputs.push(imageFeatures);
+    trainingDataOutputs.push(gatherDataState);
+
+    // Intialize array index element if currently undefined.
+    if (examplesCount[gatherDataState] === undefined) {
+      examplesCount[gatherDataState] = 0;
+    }
+    examplesCount[gatherDataState]++;
+
+    STATUS.innerText = "";
+    for (let n = 0; n < CLASS_NAMES.length; n++) {
+      STATUS.innerText +=
+        CLASS_NAMES[n] + " data count: " + examplesCount[n] + ". ";
+    }
+    window.requestAnimationFrame(dataGatherLoop);
+  }
+}
+
+/**
+ * Handle Data Gather for button mouseup/mousedown.
+ **/
 function gatherDataForClass() {
-  // TODO: Fill this out later in the codelab!
+  let classNumber = parseInt(this.getAttribute("data-1hot"));
+  gatherDataState =
+    gatherDataState === STOP_DATA_GATHER ? classNumber : STOP_DATA_GATHER;
+  dataGatherLoop();
 }
 
 /**
